eb987cd057174631ff49b0b037b489caa10f04cd
Safa <safaanwar2211@gmail.com>
2023-08-22 23:47:30 -0700

    7 commit

--- lib/Screens/home_screen.dart
+++ lib/Screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:simple_todo_app/Widgets/todo_item.dart';
 
 import '../Constants/colors.dart';
 import '../Model/todo.dart';
@@ -29,7 +30,7 @@ class _HomePageState extends State<HomePage> {
       body: Stack(
         children: [
           Container(
-            color: Colors.blue,
+
             padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
             child: Column(
               children: [
@@ -38,19 +39,78 @@ class _HomePageState extends State<HomePage> {
                   child: ListView(
                     children: [
                       Container(
-
                         margin: EdgeInsets.only(top: 50, bottom: 20),
-                       // color: Colors.black,
+                        // color: Colors.black,
                         child: Text(
-                          'All tasks ToDo',
+                          'Safa Anwar, TODO List',
                           style: TextStyle(
                             fontSize: 30,
                             fontWeight: FontWeight.w500,
                           ),
                         ),
                       ),
-
+                      for (ToDo todoo in _foundToDo.reversed)
+                        ToDoItem(
+                            todo: todoo,
+                            onToDoChanged: _handleToDoChange,
+                            onDeleteItem: _deleteToDoItem)
                     ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    margin: EdgeInsets.only(
+                      bottom: 20,
+                      right: 20,
+                      left: 20,
+                    ),
+                    padding: EdgeInsets.symmetric(horizontal: 20,
+                    vertical: 5),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey,
+                          offset: Offset(0.0,0.0),
+                          blurRadius: 10,
+                          spreadRadius: 0.0,
+                        )
+                      ],
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: TextField(
+                      controller: _todoController,
+                      decoration: InputDecoration(
+                        hintText: 'Add a new todo item',
+                        border: InputBorder.none,
+                      ),
+                    ),
+                  ),
+
+                ),
+                Container(
+                  margin: EdgeInsets.only(bottom: 20, right: 20),
+                  child: ElevatedButton(
+                    onPressed: () {
+                      _addToDoItem(_todoController.text);
+                    },
+                    child: Text(
+                      '+',style: TextStyle(
+                      fontSize: 40,
+                    ),
+                    ),
+                    style: ElevatedButton.styleFrom(
+                      primary:tdBlue,
+                      minimumSize: Size(60, 60),
+                      elevation: 10
+                    ),
                   ),
                 ),
               ],
@@ -59,6 +119,28 @@ class _HomePageState extends State<HomePage> {
         ],
       ),
     );
+  }
+
+  void _handleToDoChange(ToDo todo) {
+    setState(() {
+      todo.isDone = !todo.isDone;
+    });
+  }
+
+  void _deleteToDoItem(String id) {
+    setState(() {
+      todosList.removeWhere((item) => item.id == id);
+    });
+  }
+
+  void _addToDoItem(String toDo) {
+    setState(() {
+      todosList.add(ToDo(
+          id: DateTime.now().microsecondsSinceEpoch.toString(),
+          todoText: toDo,
+          isDone: false));
+    });
+    _todoController.clear();
   }
 
   AppBar _buildAppBar() {
