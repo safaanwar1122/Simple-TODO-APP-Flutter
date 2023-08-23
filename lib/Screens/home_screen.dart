17f496c8d9da62a0915ba31a196ddd60b2abbf54
Safa <safaanwar2211@gmail.com>
2023-08-22 20:45:40 -0700

    4 commit

--- lib/Screens/home_screen.dart
+++ lib/Screens/home_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:simple_todo_app/Model/todo.dart';
 
+import '../Constants/colors.dart';
+
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
@@ -9,18 +11,57 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  final todoList = ToDo.todoList();
+  final todosList = ToDo.todoList();
   List<ToDo> _foundToDo = [];
   final _todoController = TextEditingController();
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
-    _foundToDo = todoList;
+    _foundToDo = todosList;
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: tdBGColor,
+      appBar: _buildAppBar(),
+      body: Stack(
+        children: [
+          Container(
+            color: Colors.red,
+            padding: EdgeInsets.symmetric(horizontal: 20,vertical: 15),
+          ),
+        ],
+      ),
+    );
+
+  }
+
+  AppBar _buildAppBar() {
+    return AppBar(
+      backgroundColor: tdBGColor,
+      elevation: 0,
+      title: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Icon(
+            Icons.menu,
+            color: tdBlack,
+            size: 30,
+          ),
+          Container(
+            height: 40,
+            width: 40,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: CircleAvatar(
+                child: Image.asset('images/profile.jfif'),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
